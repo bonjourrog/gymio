56b40866a27d107eb81dadada58fbb0be8331ac7
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-03 13:33:22 -0700

    feat: add new fieldsin package store
    _package, set package and addPackage

--- app/store/packageStore.ts
+++ app/store/packageStore.ts
@@ -3,10 +3,20 @@ import { Package } from "../entity/package";
 
 export interface PackageState{
     packages:Package[];
+    _package:Package;
     setPackages:(packages:Package[])=>void;
+    setPackage:(_package:Package)=>void;
+    addPackage:(newPackage:Package)=>void;
 }
 
 export const usePackageStore = create<PackageState>(set=>({
     packages:[],
-    setPackages:(packages:Package[])=>set({packages})
+    setPackages:(packages:Package[])=>set({packages}),
+    addPackage:(newPackage:Package)=>{
+        set(state=>({
+            packages:[...state.packages, newPackage]
+        }))
+    },
+    _package:{} as Package,
+    setPackage:(_package:Package)=>set({_package})
 }))
