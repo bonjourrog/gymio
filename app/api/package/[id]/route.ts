02c8554c20b3d2d54f9d1ac0a9a70d2e1888cdf2
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-03 13:34:43 -0700

    feat: add api route for package/id

---
+++ app/api/package/[id]/route.ts
@@ -0,0 +1,76 @@
+import { supabase } from "@/app/lib/supabaseClient";
+import { ApiResponse } from "@/app/types/api";
+import { NextResponse } from "next/server";
+
+interface RouteParams {
+    id: string;
+}
+
+export async function DELETE(request: Request, { params }: { params: RouteParams }) {
+    try {
+        const { id } = await params;
+        if (!id) {
+            return NextResponse.json({ success: false, message: 'No se recibió ID' }, { status: 400 });
+        }
+        const { data, error } = await supabase.from('packages').delete().eq('id', id)
+
+        if (error) {
+            const res: ApiResponse = {
+                success: false,
+                message: error.message,
+                type: 'server'
+            }
+            return NextResponse.json(res, { status: 500 })
+        }
+        const res: ApiResponse = {
+            success: true,
+            message: 'package registered successfully',
+            type: 'success',
+            data: data,
+        }
+        return NextResponse.json(res, { status: 200 })
+    } catch (error: any) {
+        const res: ApiResponse = {
+            success: false,
+            message: error.message,
+            type: 'server',
+            code: '',
+        }
+        return NextResponse.json(res, { status: 500 })
+    }
+}
+export async function PUT(req: Request, { params }: { params: RouteParams }) {
+    try {
+        const { id } = await params;
+        const body = await req.json()
+        if (!id) {
+            return NextResponse.json({ success: false, message: 'No se recibió ID' }, { status: 400 });
+        }
+        const { data, error } = await supabase.from('packages').update(body).eq('id', id)
+        if (error) {
+            const res: ApiResponse = {
+                success: false,
+                message: error.message,
+                type: 'server',
+                code: '',
+            }
+            return NextResponse.json(res, { status: 500 })
+        }
+        const res: ApiResponse = {
+            success: true,
+            message: 'package upadted successfully',
+            type: 'success',
+            code:'PACKAGE_UPDATED',
+            data: data,
+        }
+        return NextResponse.json(res, { status: 200 })
+    } catch (error: any) {
+        const res: ApiResponse = {
+            success: false,
+            message: error.message,
+            type: 'server',
+            code: '',
+        }
+        return NextResponse.json(res, { status: 500 })
+    }
+}
