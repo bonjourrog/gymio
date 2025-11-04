165148f9a0c2236695f71cb5c244d3427a06f932
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-04 08:50:47 -0700

    feat(formatting): create spanish date formatter utitlity

--- app/lib/formatting.ts
+++ app/lib/formatting.ts
@@ -1,8 +1,10 @@
+import { Dayjs } from 'dayjs';
+
 const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 0
-    })
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+})
 export const handlePriceFormat = (value: string) => {
     const raw = value.replace(/[^0-9]/g, '')
     if (raw === '') {
@@ -11,3 +13,7 @@ export const handlePriceFormat = (value: string) => {
     const formatPrice = formatter.format(parseInt(raw, 10))
     return formatPrice;
 }
+export const spanishFormat = (value: Dayjs | null): string => {
+    if (!value) return '';
+    return value.format('ddd D [de] MMM');
+};
