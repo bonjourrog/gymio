53f22c3e8caaecaf7e634b2a25f5a41a9cba674d
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-09 16:04:06 -0700

    feat: create action for login

---
+++ app/auth/signin/action.ts
@@ -0,0 +1,29 @@
+'use server'
+import { createClient } from "@/app/lib/supabaseServerClient";
+import { ApiResponse } from "@/app/types/api";
+import { redirect } from "next/navigation";
+
+export async function login(formData: FormData) {
+    console.log('Serveeer action');
+
+    const supabase = await createClient()
+    const data = { email: formData.get('email') as string, password: formData.get('password') as string, }
+    const { error, data: userData } = await supabase.auth.signInWithPassword(data)
+    if (error) {
+        const res: ApiResponse = {
+            message: error.message,
+            success: false,
+            code: error.code,
+        }
+        return res;
+    }
+    const user = userData.user
+    const { data: profile } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", user.id)
+        .single()
+
+    if(!profile)redirect('/onboarding')
+    redirect('/dashboard')
+}
