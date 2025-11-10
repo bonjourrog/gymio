a2d68b6ee9e8dd32484ed51cecb87faca607b057
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-10 13:42:26 -0700

    refactor: remove gym name and phone fields in signup form

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,4 +1,5 @@
 'use server'
+import Link from "next/link";
 import { signup } from "./actions";
 
 
@@ -9,12 +10,9 @@ interface SearchParams {
 export default async function SignUpPage({ searchParams }: {searchParams:SearchParams}) {
     let {error} =  await searchParams;
     return <main className="flex items-center justify-center h-screen">
-        <form action={signup} className="flex flex-col gap-4 w-96 p-10 rounded-2xl border border-zinc-200 bg-linear-to-b from-blue-200 via-white to-white">
+        <form action={signup} className="flex flex-col gap-4 w-96 p-10 rounded-2xl border border-zinc-200">
+        <h2 className="text-3xl font-bold">Registro</h2>
         {error && <div className="text-red-400 font-bold">{error}</div>}
-            <label htmlFor="gym_name">
-                Nombre del gymnasio
-                <input type="text" name="gym_name" id="gym_name" />
-            </label>
             <label htmlFor="email">
                 Correo
                 <input type="email" name="email" id="email" />
@@ -27,11 +25,8 @@ export default async function SignUpPage({ searchParams }: {searchParams:SearchP
                 Confirmar contraseña
                 <input type="password" name="confirm_password" id="confirm_password" />
             </label>
-            <label htmlFor="phone">
-                Telefono
-                <input type="tel" name="phone" id="phone" />
-            </label>
-            <button className="cursor-pointer">Registrarse</button>
+            <button className="p-3 rounded-lg bg-zinc-800 text-white cursor-pointer">Registrarse</button>
+            <Link className="mx-auto" href='/auth/signin'>Iniciar sesión</Link>
         </form>
     </main>
 }
