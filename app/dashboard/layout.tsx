39a653a93f8875156de62ee21b98d5478ff81f33
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-10 13:37:15 -0700

    feat: implement a simple logout button in dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { usePathname } from 'next/navigation';
 import styles from './style.module.css';
 import { DollarSign, LayoutDashboard, LucideBoxes, Menu, PieChart, Users, X } from "lucide-react";
 import Link from 'next/link';
+import { logout } from '../actions/logout/action';
 
 const navItems = [
     { href: '/dashboard', label: 'Dashboard', icon: LayoutDashboard },
@@ -18,7 +19,9 @@ export default function DashboardLayout({ children }: Readonly<{ children: React
         if (href === '/dashboard') return pathName === '/dashboard'
         return pathName.startsWith(href)
     }
-
+    const handleLogout = async()=>{
+        await logout()
+    }
     return <main className="flex max-h-screen overflow-hidden h-screen text-zinc-700">
         <input type="checkbox" id="menu-toggle" className={styles["menu-checkbox"]}/>
         <label htmlFor="menu-toggle" className={styles["menu-icon"]}>☰</label>
@@ -45,6 +48,7 @@ export default function DashboardLayout({ children }: Readonly<{ children: React
         <section className="w-full">
             <header className="flex justify-between p-10">
                 <h1 className="text-3xl font-semibold">Dashboard</h1>
+                <button onClick={handleLogout}>Cerrar sesion</button>
                 <div className="flex items-center gap-2 p-3 rounded-full bg-zinc-100">
                     <span className="flex items-center justify-center w-8 h-8 bg-zinc-900 text-white rounded-full text-sm font-semibold">R</span>
                     <p>Perfil</p>
