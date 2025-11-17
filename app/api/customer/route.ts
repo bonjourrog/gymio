f0c5d1c4fef9be2e5f52fb4395ea5c2688c4689e
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-17 08:26:49 -0700

    feat(api): implement data sorting on customer POST route

--- app/api/customer/route.ts
+++ app/api/customer/route.ts
@@ -6,11 +6,11 @@ import { ApiResponse } from '@/app/types/api';
 export async function POST(request: Request) {
     try {
         const supabaseServer = await createClient()
-        const {data:{user}} = await supabaseServer.auth.getUser();
+        const { data: { user } } = await supabaseServer.auth.getUser();
         if (!user) return new Response("Unauthorized", { status: 401 });
-        
+
         const body = await request.json() as Customer[]
-        const customers:Customer[] = body.map(customer=>({...customer, owner_id:user?.id}))
+        const customers: Customer[] = body.map(customer => ({ ...customer, owner_id: user?.id }))
         const { data, error } = await supabase.from('customers').insert(customers).select();
         if (error) {
             const res: ApiResponse = {
@@ -47,20 +47,28 @@ export async function GET(request: Request) {
                 name,
                 phone,
                 membership_customers (
+                membership_id,
                 memberships (
                     id,
                     start_date,
                     end_date,
+                    status,
                     packages (
                     id,
                     name,
-                    price
+                    price,
+                    group_size
                     )
                 )
                 )
             `);
-            
-        // const { data, error } = await supabase.from('customers').select();
+
+            const sorted = data?.sort((a, b) => {
+                const aId = a.membership_customers[0]?.membership_id || '';
+                const bId = b.membership_customers[0]?.membership_id || '';
+                return bId.localeCompare(aId);
+            });
+
         if (error) {
             const res: ApiResponse = {
                 success: false,
@@ -73,7 +81,7 @@ export async function GET(request: Request) {
             success: true,
             message: 'success',
             type: 'server',
-            data:data
+            data: sorted
         }
         return Response.json(res, { status: 500 })
     } catch (error: any) {
