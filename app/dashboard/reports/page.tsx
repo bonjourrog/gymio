4f3287e262c96fb995ce7f816599414afa4f0885
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-11-17 11:52:43 -0700

    feat: add reports page
    - no layout

---
+++ app/dashboard/reports/page.tsx
@@ -0,0 +1,5 @@
+export default function ReportsPage() {
+    return <main className="flex flex-col gap-4 h-screen w-[calc(100vw-13em)] p-10">
+        <h1 className="font-bold text-3xl">Reportes</h1>
+    </main>
+}
