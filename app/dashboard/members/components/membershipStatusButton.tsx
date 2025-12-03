594b63fbe9f1774ff834cb8020b67aca697c35d4
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-12-02 20:08:57 -0700

    refactor: display only meni item for membership with active and expired status

--- app/dashboard/members/components/membershipStatusButton.tsx
+++ app/dashboard/members/components/membershipStatusButton.tsx
@@ -31,6 +31,9 @@ function MembershipStatus({ membership }: { membership?: Membership | undefined 
                 case 'canceled':
                     content = { text: 'Cancelada', styles: 'bg-gray-200 text-zinc-500' }
                     break;
+                case 'refunded':
+                    content = { text: 'Reembolsado', styles: 'bg-#ec9e50-200 text-zinc-500' }
+                    break;
             }
         }
         return content;
@@ -50,7 +53,7 @@ function MembershipStatus({ membership }: { membership?: Membership | undefined 
         }
     }
     return <>
-        {showMenu && <div onClick={() => setShowMenu(false)} className={`fixed w-screen h-screen top-0 left-0 ${loading ? 'z-20' : 'z-10'} ${styles.noise}`}>
+        {showMenu && <div onClick={() => setShowMenu(false)} className={`fixed w-screen h-screen top-0 left-0 ${loading ? 'z-20' : 'z-10'} bg-zinc-900/30`}>
         </div>}
         {loading && <div className={styles["sk-chase"]}>
             <div className={styles["sk-chase-dot"]}></div>
@@ -60,7 +63,7 @@ function MembershipStatus({ membership }: { membership?: Membership | undefined 
             <div className={styles["sk-chase-dot"]}></div>
             <div className={styles["sk-chase-dot"]}></div>
         </div>}
-        <span onClick={() => (membership?.status==='canceled' || membership?.status === "expired")&&setShowMenu(true)} className={`${translateStatus(membership?.status!).styles} relative rounded-lg px-4 py-2 font-light cursor-pointer`}>
+        <span onClick={() => (membership?.status !== "refunded") && setShowMenu(true)} className={`${translateStatus(membership?.status!).styles} relative rounded-lg px-4 py-2 font-light cursor-pointer`}>
             {
                 showMenu && <div className="absolute left-0 -top-6 flex flex-col gap-2 min-w-20 min-h-20 bg-white rounded-xl p-4 z-20 text-zinc-700">
                     {
@@ -68,7 +71,12 @@ function MembershipStatus({ membership }: { membership?: Membership | undefined 
                             <span onClick={() => updateStatus('active')} key={'active'} className={`py-2 px-6 rounded-md text-sm font-bold ${translateStatus('active').styles}`}>Activar</span>
 
                             <span onClick={() => updateStatus('canceled')} key={'canceled'} className={`py-2 px-6 rounded-md text-sm font-bold ${translateStatus('canceled').styles}`}>Cancelar</span>
-                        </> : membership?.status === 'canceled' ? <span onClick={() => updateStatus('active')} key={'active'} className={`py-2 px-6 rounded-md text-sm font-bold ${translateStatus('active').styles}`}>Activar</span> : null
+                        </> :
+                            membership?.status === "active" ? <>
+                                <span onClick={() => updateStatus('canceled')} key={'canceled'} className={`py-2 px-6 rounded-md text-sm font-bold ${translateStatus('canceled').styles}`}>Cancelar</span>
+
+                                <span onClick={() => updateStatus('refunded')} key={'refunded'} className={`py-2 px-6 rounded-md text-sm font-bold ${translateStatus('refunded').styles}`}>Reembolso</span>
+                            </> : null
                     }
                 </div>
             }
