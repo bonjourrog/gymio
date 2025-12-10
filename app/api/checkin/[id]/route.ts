990523e149cfbae3d9a275ac03b718a7aaa8a298
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-12-10 16:27:21 -0700

    feat: add DELETE endpoint by id for checkin

---
+++ app/api/checkin/[id]/route.ts
@@ -0,0 +1,33 @@
+import { supabase } from "@/app/lib/supabaseClient";
+import { ApiResponse } from "@/app/types/api";
+import { NextResponse } from "next/server";
+
+interface RouteParams {
+    id: string;
+}
+export async function DELETE(request: Request ,{params}: {params:RouteParams} ) {
+    try {
+        const {id} = await params;
+        const { data, error } = await supabase.from('check_ins').delete().eq('id', id);
+        if (error) {
+            
+            throw new Error('ocurrio un error al eliminar la asistencia');
+        }
+        const res: ApiResponse = {
+            message: 'peticion exitosa',
+            success: true,
+            type: 'success',
+            data
+        }
+        return NextResponse.json(res)
+    } catch (error: any) {
+        const res: ApiResponse = {
+            message: error.message,
+            success: false,
+            data: [],
+            type: 'server'
+        }
+        return NextResponse.json(res);
+    }
+
+}
